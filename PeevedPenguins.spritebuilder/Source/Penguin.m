4fd4f72ad42476384b62b4b11d417f372551d2a5
sgharlow <SteveHarlow@Steves-iMac.local>
2014-07-04 12:25:58 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Penguin.m
@@ -0,0 +1,21 @@
+//
+//  Penguin.m
+//  PeevedPenguins
+//
+//  Created by Steve Harlow on 7/4/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Penguin.h"
+
+@implementation Penguin
+- (id)init {
+    self = [super init];
+    
+    if (self) {
+        CCLOG(@"Penguin created");
+    }
+    
+    return self;
+}
+@end
---
+++ PeevedPenguins.spritebuilder/Source/Seal.m
@@ -0,0 +1,21 @@
+//
+//  Seal.m
+//  PeevedPenguins
+//
+//  Created by Steve Harlow on 7/4/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Seal.h"
+
+@implementation Seal
+- (id)init {
+    self = [super init];
+    
+    if (self) {
+        CCLOG(@"Seal created");
+    }
+    
+    return self;
+}
+@end
