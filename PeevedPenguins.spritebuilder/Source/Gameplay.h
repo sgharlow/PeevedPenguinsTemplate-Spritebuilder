11b9694e4c3f2797dbb83c05758bf3e1ae5d4f8a
sgharlow <SteveHarlow@Steves-iMac.local>
2014-07-07 10:02:43 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Steve Harlow on 7/7/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
