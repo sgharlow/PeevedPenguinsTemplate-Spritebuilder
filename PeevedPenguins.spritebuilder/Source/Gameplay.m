6ac6553de86c85b5a7216e394b7de8a1f6dafc3f
sgharlow <SteveHarlow@Steves-iMac.local>
2014-07-07 10:25:30 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -10,12 +10,15 @@
 
 @implementation Gameplay {
     CCPhysicsNode *_physicsNode;
+    CCNode *_levelNode;
     CCNode *_catapultArm;
 }
 
 - (void)didLoadFromCCB {
     // tell this scene to accept touches
     self.userInteractionEnabled = TRUE;
+    CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
+    [_levelNode addChild:level];
 }
 
 // called on every touch in this scene
